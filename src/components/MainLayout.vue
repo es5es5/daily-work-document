42b48688751285171ddee44b9fb03656a48a5f73
es5es5 <es5e5s@nate.com>
2021-02-09 22:02:29 +0900

    routing

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <main class="main">
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+  max-width: 1024px;
+  margin: 0 auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,34 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Home from '@/components/Home'
+import MainLayout from '@/components/MainLayout'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/main',
     name: 'Home',
     component: Home
   },
   {
-    path: '/daily',
-    name: 'DailyList',
-    component: () => import('@/pages/DailyList.vue')
-  },
-  {
-    path: '/daily/:detailId',
-    name: 'DailyDetail',
-    component: () => import('@/pages/DailyDetail.vue')
+    path: '/main',
+    redirect: '/main/daily',
+    name: 'Main',
+    component: MainLayout,
+    children: [
+      {
+        path: 'daily',
+        name: 'DailyList',
+        component: () => import('@/pages/DailyList.vue')
+      },
+      {
+        path: 'daily/:detailId',
+        name: 'DailyDetail',
+        component: () => import('@/pages/DailyDetail.vue')
+      },
+    ]
   },
 ]
 
